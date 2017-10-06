0571fcd205827a936a14c15ed8d890a47edaa943
Brisingeros <kevin.kevin.carrasco@gmail.com>
2017-10-07 01:19:53 +0200

    Inicio del juego, con una escena que consiste en un menú principal

---
+++ Norte, Sur, Este oEste/Assets/Scripts/StartIt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { Idle, Playing }
+
+public class StartIt : MonoBehaviour {
+
+    public GameState gameState = GameState.Idle;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
