b3de8dbab49c402bbc119b307c8bfc189807bc68
Brisingeros <kevin.kevin.carrasco@gmail.com>
2017-10-07 11:12:35 +0200

    Finalización del Menú principal, a falta de enlace con el juego

--- Norte, Sur, Este oEste/Assets/Scripts/StartIt.cs
+++ Norte, Sur, Este oEste/Assets/Scripts/StartIt.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GameState { Idle, Playing }
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class StartIt : MonoBehaviour {
 
-    public GameState gameState = GameState.Idle;
+    public int contador = 0;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject[] menu;
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 1; i < menu.Length; i++) {
+            menu[i].SetActive(false);
+        }
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        bool userAction = Input.GetKeyDown("up") || Input.GetMouseButtonDown(0);
+
+        if (userAction && contador < 2)
+        {
+            for (int i = 0; i < menu.Length; i++)
+            {
+                menu[i].SetActive(false);
+            }
+
+            contador++;
+
+            //System.Threading.Thread.Sleep(1000);
+
+            menu[contador].SetActive(true);
+        }
+        else if (contador == 3) {
+
+        }
+    }
 }
